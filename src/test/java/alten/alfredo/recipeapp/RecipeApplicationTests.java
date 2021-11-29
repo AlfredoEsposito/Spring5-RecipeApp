09071a261510f08c2cb660b855ecac8e1b130833
AlfredoEsposito <espositoalf31@gmail.com>
2021-11-29 11:51:27 +0100

    some file added

---
+++ src/test/java/alten/alfredo/recipeapp/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package alten.alfredo.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
